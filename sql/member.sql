2b5b968cb660857eceafb6433426d24e280de3ea
sanghakyi <sang105kr@gmail.com>
2022-09-07 17:34:27 +0900

    220907-8

--- sql/member.sql
+++ sql/member.sql
@@ -13,6 +13,8 @@ alter table member add constraint member_member_id_pk primary key(member_id);
 --unique
 alter table member add constraint member_email_uk unique (email);
 --not null
+alter table member modify email constraint member_email_nn not null;
+alter table member modify pw constraint member_pw_nn not null;
 alter table member modify cdate constraint member_cdate_nn not null;
 alter table member modify udate constraint member_udate_nn not null;
 
--- src/main/java/com/kh/myapp3/web/admin/AdminMemberController.java
+++ src/main/java/com/kh/myapp3/web/admin/AdminMemberController.java
@@ -23,16 +23,22 @@ public class AdminMemberController {
 
   //등록화면
   @GetMapping("/add")
-  public String addForm(){
-
+  public String addForm(Model model){
+    model.addAttribute("addForm", new AddForm());
     return "admin/member/addForm";  //가입 화면
   }
   //등록처리	POST	/members/add
   @PostMapping("/add")
-  public String add(AddForm addForm){
+  public String add(@ModelAttribute AddForm addForm){
     //검증
+    //model.addAttribute("addForm", addForm);
     log.info("addForm={}",addForm);
+    if(addForm.getEmail().trim().length() == 0){
+      return "admin/member/addForm";
+    }
 
+
+    //회원등록
     Member member = new Member();
     member.setEmail(addForm.getEmail());
     member.setPw(addForm.getPw());
@@ -91,7 +97,7 @@ public class AdminMemberController {
   }
 
   //삭제처리
-  @PostMapping("/{id}/del")
+  @GetMapping("/{id}/del")
   public String del(@PathVariable("id") Long id){
     int deletedRow = adminMemberSVC.del(id);
     if(deletedRow == 0){
